600ce13c33816b7510cbb1f86089321a4d9c1731
cittantoniovaras <cittantoniovaras@gmail.com>
2020-07-28 23:00:38 +0000

    first commit yeahh

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hallo, willkommen!'
 
+# prueba recibir parametros
 @app.route('/api/<int:parametro>')
 def llamada(parametro):
     return parametro
+
+@app.route('/<string:nombre>')
+def prueba(nombre):
+    return 'Hallo '+nombre
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
