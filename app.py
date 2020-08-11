b226a1d28a6d86381a2efda20419279243352edd
Gabriel <gab.gonzalezr@alumnos.duoc.cl>
2020-08-11 23:43:16 +0000

    normalizacion repo local

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ def hello_world():
     return 'Hallo, willkommen!'
 
 # prueba recibir parametros
+# http://http//167.99.231.117:5000/api/llamada?parametro=1000
 @app.route('/api/<int:parametro>')
 def llamada(parametro):
+    respuesta_modelo = subprocess.call('python3 tutorial2.py', parametro)
+    #return (data:{var1:'valor1', var2:'valor2'})
     return parametro
 
 @app.route('/<string:nombre>')
