cc5e80b9d0f5008a391d4459c6112bfe2fc57c35
Citt Analytics <cittavaras@github.com>
2020-10-23 05:24:10 +0000

    se elimina archivo inicio.captura.sh

--- scripts/inicio-captura.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /root/Development/repositorio/analisis-datos-back/scripts
-echo 'Deteniendo ' >> captura.log
-echo date >> captura.log
-date >> captura.apagar.log
-kill $(cat captura.pid)
-python3 captura.py >> captura.log &
